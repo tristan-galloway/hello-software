0c30515547c2ed2e8a780d199cc99ede526b7d14
Tristan Galloway <tristan.a.galloway@gmail.com>
2024-04-26 19:42:40 -0600

    Added comments

--- hello_world.py
+++ hello_world.py
@@ -6,5 +6,7 @@ Goal: Create a basic hello world program
 """
 import os
 
+# Clear the console for a clear display.
 os.system('cls')
+# Print "Hello World" to the built in terminal.
 print("Hello World")
